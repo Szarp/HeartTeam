4f2cfa41247bfc32280755f8ac36d02279e58077
szarp <lizusfull@gmail.com>
2017-05-10 21:46:57 +0200

    move svg functions to new module

--- charts/lineByLine.js
+++ charts/lineByLine.js
@@ -6,7 +6,8 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     session = require('/Users/bartek/gitrepo/substitutions/myModules/userSession.js'),
     cookieParser = require('cookie-parser'),
-    fs = require('fs');
+    fs = require('fs'),
+    svg = require('/Users/bartek/gitrepo/substitutions/myModules/svgCreator');
 var app = express();
 //app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -15,7 +16,7 @@ app.use(cookieParser());
 var cookie = new session.sessionCreator();
 var fileList = {};
 var sessionList = [];
-const testFolder = __dirname+'/data';
+const testFolder = '/Users/bartek/gitrepo/HeartTeam/charts/data/';
 fs.readdir(testFolder, function(err, files){
     fileList=files;
  
@@ -56,9 +57,10 @@ app.post('/data',function(req,res){
                 pointer=1;
             }
             if(pointer==1){
-                preparePath(req.body['name'],function(path){
+                svg.svgToSend(req.body['name'],function(path){
+                        console.log(path);
                 
-                        res.send({data:path});
+                        res.send(path);
                 });
             }
             else{res.send('err');}
@@ -73,57 +75,11 @@ var last=0;
 var dPath="";
 app.get('/test',function(req,res){
     ///*
-    readAndRewrite('1_test3.txt',function(tab){
-        var i=1;
-        var last=0;
-        var mode=0;
-        var index =0;
-        var table=[0,2,2,2,0];
-        do{
-            index=0;
-            /*for(j=0;j<table.length;j++){
-                //console.log(tab[i+j]);
-                if(tab[i+j]<table[j]){
-                    index++;
-                }
-                
-            }
-            if (index==table.length){
-                mode++;
-                //console.log('a');
-            }
-            */
-            //console.log(tab[i],tab[i+4]);
-            if(tab[i]+tab[i+4]==0){
-                for(var j=0;j<5;j++){
-                    tab[i+j]=0;
-                }
-            }
-                //mode++;
-            i++;
-        }while(i+4<tab.length);
-        //console.log(mode);
-        //saveTable('1_detected.txt',tab,function(){ })
-   })
-    readAndRewrite('1_fromInternet.txt',function(tab){
-        //readAndRewrite('1_test.txt',function(tab1){
-            var i=0;
-            //var pointer;
-            do{
-                if(tab[i]>tab[i+1])
-                    tab[i]=1;
-                else
-                    tab[i]=0;
-            //last=tab[i]-tab[i+10];
-            //last/=2;
-            //tab[i]=last;
-            i++;
-        }while(i+1<tab.length);
-        //saveTable('1_test3.txt',tab,function(){ })
-        //})
-    })
+
+
         
    //*/
+    /*
     readAndRewrite('1.txt',function(tab){
         var i=2;
         var last;
@@ -135,54 +91,17 @@ app.get('/test',function(req,res){
         }while(i<tab.length);
         //saveTable('1_average.txt',tab,function(){ })
     })
-    cleanPath('1.txt','somebin.txt',function(clearData){
-        
-        
-        
-    })
-    readAndRewrite('1.txt',function(tab){
-    var range = 5; // Number of data points each side to sample.
-    var decay = 0.8; // [0.0 - 1.0] How slowly to decay from raw value.
-        //var  noisy = //read from file
-        var clean = CleanData(tab, range, decay); 
-        //var low = lowpass(tab)
-        /*
-        var high = highpass(tab)
-        var newTab=[];
-        for(var i=0;i<tab.length;i++){
-            newTab[i]=tab[i]-high[i];
-          
-          
-        }
-        //console.log(low);
-        //var inv = invert(clean);
-        newTab=newTab.slice(2);
-        */
-        //saveTable('1_fromInternet.txt',clean,function(){ })
-        //saveTable('1_fromInternet2.txt',newTab,function(){ })
-    //console.log(clean);
-    })
+    */
+    cleanPath('1.txt','1_test1.txt',function(clearData){})
+    //cleanPath('2.txt','2_test1.txt',function(clearData){})
+    //cleanPath('3.txt','3_test1.txt',function(clearData){})
+    //cleanPath('outputHR.txt','outputHR_test1.txt',function(clearData){})
+
    
     
-    readAndRewrite('1_fromInternet.txt',function(tab){
-        readAndRewrite('1_detected.txt',function(tab1){
-            var i=0;
-            var last;
-            do{
-                if(tab1[i]==0){
-                    tab[i]=0
-                }
-                //last=
-                i++;
-            }while(i<tab.length);
-            saveTable('1_test2.txt',tab,function(){ })
 
-
-        })
-        
-    })
     
-    
+    /*
     readAndRewrite('1_test2.txt',function(tab){
         var i=0;
         var begIndex=0;
@@ -221,6 +140,7 @@ app.get('/test',function(req,res){
         //console.log(i);
         //saveTable('test.txt',tab,function(){ })
     })
+    */
     /*
     readAndRewrite('test_min.txt',function(tab){
         var i=0;
@@ -242,40 +162,39 @@ app.listen(3000,function(){
 });
 // Console will print the message
 var table1=[];
-function saveTable(destination,table,callback){
-    var stream = fs.createWriteStream(__dirname+'/data/'+destination);
-    stream.once('open',function(fd){
-        for(var i=0;i<table.length;i++){
-            stream.write(table[i]+'\n');
-        }
-        stream.end();
-        setImmediate(function() {
-                callback();
-        });
-    })
-    
-}
+
+//allTogather('1_test1.txt','1_test2.txt');
+//allTogather('2_test1.txt','2_test2.txt');
+//allTogather('3_test1.txt','3_test2.txt');
+//allTogather('outputHR_test1.txt','outputHR_test2.txt');
 function allTogather(from,to,callback){
-      readAndRewrite(from,function(tab){
+      svg.readAndRewrite(from,function(tab){
         var startCounting=0;
         
         var i=1;
         var last=0;
         var mode=0;
         var index =0;
-          var buff=[]
+          var buff=[];
         var table=[0,2,2,2,0];
+          console.log('hey');
         do{
             index=0;
-            if(tab[i]<tab[i+1]){ //szukamy jedynki
+            if(tab[i]>tab[i+1]){ //szukamy jedynki
                 buff[0]=i;
                 do{
                     
                     
                     i++;
-                }while(tab[i]<tab[i+1])
-                if(i-buff[0]>4){
-                    
+                }while(tab[i]>=tab[i+1])
+                    //console.log(i-buff[0]);
+                if(i-buff[0]>3){
+                    console.log(Math.sqrt((i-buff)*Math.pow(tab[buff]-tab[i],2)));
+                    var a= Math.sqrt((i-buff)*Math.pow(tab[buff]-tab[i],2));
+                    if(a>100){
+                        tab[buff]=100000;
+                        tab[i]=100000;
+                    }
                     
                 }
                 
@@ -296,44 +215,17 @@ function allTogather(from,to,callback){
             }
             */
             //console.log(tab[i],tab[i+4]);
-            if(tab[i]+tab[i+4]==0){
-                for(var j=0;j<5;j++){
-                    tab[i+j]=0;
-                }
-            }
                 //mode++;
             i++;
-        }while(i+4<tab.length);
+        }while(i<tab.length);
         //console.log(mode);
-        //saveTable('1_detected.txt',tab,function(){ })
+        svg.saveTable(to,tab,function(){ })
    })
     
     
     
 }
-    
-function preparePath(destination,callback){
-    var somePath="M0 0";
-    //var min=10108;
-    var index1=0;
-    var file = readline.createInterface({
-        input: fs.createReadStream(__dirname+'/data/'+destination)
-    });
-    file.on('line',function(line){
-        index1++;
-        parsedLine=Number(line);
-        somePath+=makePoint(index1,parsedLine);
-    })
-    file.on('close', function (line) {
-            //console.log(dPath);
-            setImmediate(function() {
-                callback(somePath);
-            });
-       // console.log(
-    
-    
-    })
-}
+
 //prepareSpriral();
 function prepareSpriral(){
     var string='M0 0';
@@ -350,29 +242,7 @@ function getSpiral(r,d){
     return [x,y];
     
 }
-function readAndRewrite(from,callback){
-    var file = readline.createInterface({
-        input: fs.createReadStream(__dirname+'/data/'+from)
-    });
-    var table=[];
-    last=0;
 
-        file.on('line', function(line){
-            table[table.length]=parsedLine=Number(line);
-        });    
-        file.on('close', function (line) {
-            //console.log(dPath);
-            //stream.end();
-            setImmediate(function() {
-                callback(table);
-            });
-       // console.log(min,max);
-           // console.log(fd);
-          //stream.write("My first row\n");
-          //stream.write("My second row\n");
-          //stream.end();
-    });
-}
 function makePoint(index,yPos){
     return ' L'+index*1+' '+yPos;
 }
@@ -402,6 +272,7 @@ function substarctMin(min,callback){
     });
     
 }
+/*
 function minMaxValue(callback){
     var min;
     var max;
@@ -428,50 +299,20 @@ function minMaxValue(callback){
     });
     
 }
-
-
-var invert = function (h) {
-    var cnt;
-    for (cnt = 0; cnt < h.length; cnt++) {
-      h[cnt] = -h[cnt]
-    }
-    h[(h.length - 1) / 2]++
-    return h
-  }
- 
-//high pass
-function lowpass(data){
-    var cutoff=0.8;
-    var RC = 1.0/(cutoff*2*3.14);
-    var dt = 1.0/100;
-    var alpha = dt/(RC+dt);
-    var filteredArray=new Array(data.length);
-        filteredArray[0] = data[0];
-    for(var i=1; i<data.length; i++){
-        filteredArray[i] = filteredArray[i-1] + (alpha*(data[i] - filteredArray[i-1]));
-    }
-    return filteredArray;
-}
-
-    function highpass(data){
-        var cutoff=100;
-        var RC = 1.0/(cutoff*2*3.14);
-        var dt = 1.0/100;
-        var alpha = dt/(RC+dt);
-        var filteredArray=new Array(data.length);
-            filteredArray[0] = data[0];
-        for(var i=1; i<data.length; i++){
-            filteredArray[i] = alpha * (filteredArray[i-1] + data[i] - data[i-1]);
-        }
-         return filteredArray;
-
-    }
+*/
     function cleanPath(from,to,callback){
-        readAndRewrite(from,function(tab){
-            var range = 5; // Number of data points each side to sample.
-            var decay = 0.8; // [0.0 - 1.0] How slowly to decay from raw value.
-                var clean = CleanData(tab, range, decay); 
-                saveTable(to,clean,function(){
+        svg.readAndRewrite(from,function(tab){
+            var range = 4; // Number of data points each side to sample.
+            var decay = 0.9; // [0.0 - 1.0] How slowly to decay from raw value.
+            var clean = CleanData(tab, range, decay); 
+            //console.log(clean);
+            var x=Math.floor();
+            var i=0;
+            do{
+                clean[i]=Math.floor(clean[i]);
+                i++;
+            }while(i<clean.length);
+                svg.saveTable(to,clean,function(){
                     setImmediate(function() {
 						callback(clean);
 					});
@@ -480,7 +321,56 @@ function lowpass(data){
             //console.log(clean);
         })
     }
-    
+var buff = new Array(24);
+readSixteen('1.txt','test.txt');
+function readSixteen(from,to){
+    cleanPath(from,to,function(tab){
+        //var buff= new Array(16);
+        var all=[0,0,0,0];
+        //console.log(Math.floor(tab.length/16));
+        for(var i=0;i<Math.floor(tab.length/16);i++){
+            
+            for(var j=8;j<24;j++){
+                buff[j]=tab[16*i+j];
+            }
+        //console.log(buff);
+        //console.log(all.concat(newCleanData()));
+        all=all.concat(newCleanData());
+        }
+        svg.saveTable(to,all,function(){})
+        //console.log(all);
+    })
+}
+//apply values
+
+//do something
+
+//last 4 values =
+    function newCleanData(){
+        var range = 4;
+        var decay = 0.9;
+        var coefficients = Coefficients(range, decay);
+        var clean = new Array(16);
+        // Calculate divisor value.
+        var divisor = 0;
+        for (var i = -range; i <= range; i++)
+            divisor += coefficients[Math.abs(i)];
+ 
+        for (var i = range; i < 20; i++)
+        {
+            var temp = 0;
+            for (var j = -range; j <= range; j++)
+                temp += buff[i + j] * coefficients[Math.abs(j)];
+            clean[i-range] = Math.floor(temp / divisor);
+        }
+        for(var i=0;i<8;i++){
+            buff[i]=buff[15+i];
+        }
+        console.log(clean);
+        return clean;  
+    }
+
+
     function CleanData(noisy,range,decay)
     {
         var clean = new Array(noisy.length);
@@ -501,6 +391,7 @@ function lowpass(data){
         }
  
         // Calculate leading and trailing slopes.
+        
         var leadSum = 0;
         var trailSum = 0;
         var leadRef = range;
@@ -519,6 +410,7 @@ function lowpass(data){
             clean[leadRef - i] = clean[leadRef] + leadSlope * i;
             clean[trailRef + i] = clean[trailRef] + trailSlope * i;
         }
+        
         return clean;
     }
  
